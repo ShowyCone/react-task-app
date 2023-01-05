5df7e18a7302f35375455b5ac9b3f2868bd60bc0
Rafael Alejandro Arciniega <79430414+ShowyCone@users.noreply.github.com>
2023-01-05 07:43:58 -0400

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html, js, ts, vue}", 
+    "./src/**/*"
   ],
   plugins: [],
 }
